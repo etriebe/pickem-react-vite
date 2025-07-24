ff76f87105c9be91756f88848c239ed4b87d2bb4
Eric Triebe <eric.triebe@gmail.com>
2025-07-24 10:33:18 -0700

    Add main grid and nav bar

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -7,12 +7,16 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import SideMenu
   from "./components/SideMenu";
+import AppNavbar from "./components/AppNavbar";
+import Header from "./components/Header";
+import CssBaseline from '@mui/material/CssBaseline';
+import AppTheme from "./theme/AppTheme";
+import MainGrid from "./components/MainGrid";
 import "./App.css";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,79 +24,39 @@ function App() {
 
   return (
     <>
-      <CssBaseline />
-      <Box sx={{ display: 'flex' }}>
-        <SideMenu />
-        {/* Main content */}
-        <Box
-          component="main"
-          sx={(theme) => ({
-            flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
-            overflow: 'auto',
-          })}
-        >
-          <Stack
-            spacing={2}
-            sx={{
-              alignItems: 'center',
-              mx: 3,
-              pb: 5,
-              mt: { xs: 8, md: 0 },
-            }}
+      <AppTheme>
+
+        <CssBaseline />
+        <Box sx={{ display: 'flex' }}>
+          <SideMenu />
+          <AppNavbar />
+          {/* Main content */}
+          <Box
+            component="main"
+            sx={(theme) => ({
+              flexGrow: 1,
+              backgroundColor: theme.vars
+                ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                : alpha(theme.palette.background.default, 1),
+              overflow: 'auto',
+            })}
           >
-          </Stack>
+            <Stack
+              spacing={2}
+              sx={{
+                alignItems: 'center',
+                mx: 3,
+                pb: 5,
+                mt: { xs: 8, md: 0 },
+              }}
+            >
+            </Stack>
+            <Header />
+            <MainGrid />
+          </Box>
         </Box>
-      </Box>
-      <ButtonUsage />
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://hono.dev/" target="_blank">
-          <img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-        </a>
-        <a href="https://workers.cloudflare.com/" target="_blank">
-          <img
-            src={cloudflareLogo}
-            className="logo cloudflare"
-            alt="Cloudflare logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React + Hono + Cloudflare</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label="increment"
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="card">
-        <button
-          onClick={() => {
-            fetch("/api/")
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name));
-          }}
-          aria-label="get name"
-        >
-          Name from API is: {name}
-        </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the logos to learn more</p> */}
+        <ButtonUsage />
+      </AppTheme>
     </>
   );
 }
---
+++ src/react-app/components/MainGrid.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import HighlightedCard from './HighlightedCard';
+import { Button } from '@mui/material';
+
+export default function MainGrid() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
+      {/* cards */}
+      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+        Overview
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        columns={12}
+        sx={{ mb: (theme) => theme.spacing(2) }}
+      >
+        <HighlightedCard />
+        <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
+          <HighlightedCard />
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <HighlightedCard />
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <HighlightedCard />
+        </Grid>
+      </Grid>
+      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+        Details
+      </Typography>
+      <Grid container spacing={2} columns={12}>
+        <Grid size={{ xs: 12, lg: 9 }}>
+          <Button />
+        </Grid>
+        <Grid size={{ xs: 12, lg: 3 }}>
+          <Stack gap={2} direction={{ xs: 'column', sm: 'row', lg: 'column' }}>
+            <Button />
+            <Button />
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
