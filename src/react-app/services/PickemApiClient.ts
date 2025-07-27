732f1159665b20ad92320c0284cac4d2689e3cc5
Eric Triebe <eric.triebe@gmail.com>
2025-07-26 21:59:23 -0700

    Add new APIs

--- src/react-app/services/PickemApiClient.ts
+++ src/react-app/services/PickemApiClient.ts
@@ -1512,6 +1512,39 @@ export class Client {
     /**
      * @return OK
      */
+    isAuthorized(): Promise<void> {
+        let url_ = this.baseUrl + "/api/user/IsAuthorized";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "GET",
+            headers: {
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processIsAuthorized(_response);
+        });
+    }
+
+    protected processIsAuthorized(response: Response): Promise<void> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+                return;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+                return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<void>(null as any);
+    }
+
+    /**
+     * @return OK
+     */
     getUserSettingFromUserId(): Promise<void> {
         let url_ = this.baseUrl + "/api/user/GetUserSettingFromUserIdAsync";
         url_ = url_.replace(/[?&]$/, "");
