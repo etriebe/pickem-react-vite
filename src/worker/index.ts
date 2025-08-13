426316bf481de4d587b7d92fc0d6847b0822c18b
Eric Triebe <eric.triebe@gmail.com>
2025-08-13 11:24:12 -0700

    Uncomment worker API

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
-// app.get("/api2/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/api2/", (c) => c.json({ name: "Cloudflare" }));
 export default app;
