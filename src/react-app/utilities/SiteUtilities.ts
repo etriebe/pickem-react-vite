bd982e0a7c0a87868d83cc353314c40dde576674
Eric Triebe <eric.triebe@gmail.com>
2025-08-22 09:56:04 -0700

    Try and use camel case

--- src/react-app/utilities/SiteUtilities.ts
+++ src/react-app/utilities/SiteUtilities.ts
@@ -106,9 +106,9 @@ export class SiteUtilities {
     }
 
     static getTeamIconPathFromTeam(team: TeamDTO, league: LeagueDTO): string {
-        const city = team.city?.replace(" ", "_").toLocaleLowerCase();
-        const name = team.name?.replace(" ", "_").toLocaleLowerCase();
-        const imagePath = `/teamicons/${SiteUtilities.getSportFolderNameFromSportNumber(league?.sport!).toLocaleLowerCase()}/${city}_${name}.svg`;
+        const city = team.city?.replace(" ", "_");
+        const name = team.name?.replace(" ", "_");
+        const imagePath = `/TeamIcons/${SiteUtilities.getSportFolderNameFromSportNumber(league?.sport!)}/${city}_${name}.svg`;
         return imagePath;
     }
 
