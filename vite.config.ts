5efa4576233007f44199c4569803f8855d07733c
Eric Triebe <eric.triebe@gmail.com>
2025-09-05 16:07:18 -0700

    Change to use api2

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [react(), mkcert()],
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://localhost:32773',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 });
