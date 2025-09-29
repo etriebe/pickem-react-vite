5cdc91e287c76e09db85d8a7af1371ba04d94493
Eric Triebe <eric.triebe@gmail.com>
2025-09-29 16:28:03 -0700

    Remove unused import

--- src/react-app/components/AppRouter.tsx
+++ src/react-app/components/AppRouter.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Routes, Route } from 'react-router';
 import { AuthenticationUtilities } from '../utilities/AuthenticationUtilities';
 import MyLeagues from './MyLeagues';
