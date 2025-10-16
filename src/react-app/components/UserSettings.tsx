b055575e94be95c9376640113f0001d823732dd2
Eric Triebe <eric.triebe@gmail.com>
2025-10-16 13:09:40 -0700

    Improve sign in performance by not having retry

---
+++ src/react-app/components/UserSettings.tsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+class UserSettings extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>UserSettings</div>
+    )
+  }
+}
+
+export default UserSettings
