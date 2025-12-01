844a2fa0c965c268b6aa7c47a1cede64f9e430e2
Eric Triebe <eric.triebe@gmail.com>
2025-12-01 11:37:32 -0800

    Fix sign-in look and format and make header button go to home page

--- src/react-app/components/AppNavbar.tsx
+++ src/react-app/components/AppNavbar.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import MuiToolbar from '@mui/material/Toolbar';
 import { tabsClasses } from '@mui/material/Tabs';
 import Typography from '@mui/material/Typography';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
 import SideMenuMobile from './SideMenuMobile';
 import MenuButton from './MenuButton';
 import ColorModeIconDropdown from '../theme/ColorModeIconDropdown';
 import { SideMenuProps } from './SideMenu';
+import siteLogo from '../assets/logo.png';
 
 const Toolbar = styled(MuiToolbar)({
   width: '100%',
@@ -29,11 +28,14 @@ const Toolbar = styled(MuiToolbar)({
   },
 });
 
-export default function AppNavbar({ isAuthenticated, username, email } : SideMenuProps ) {
+export default function AppNavbar({ isAuthenticated, username, email }: SideMenuProps) {
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen);
+  };
+  const goHome = () => () => {
+    window.location.href = '/';
   };
 
   return (
@@ -63,8 +65,14 @@ export default function AppNavbar({ isAuthenticated, username, email } : SideMen
             direction="row"
             spacing={1}
             sx={{ justifyContent: 'center', mr: 'auto' }}
+            onClick={goHome()}
+            className='centerDivContainer'
           >
-            <CustomIcon />
+            <img
+              src={siteLogo}
+              alt="Site Logo"
+              style={{ width: 30, height: 30, alignSelf: 'center' }}
+            />
             <Typography variant="h4" component="h1" sx={{ color: 'text.primary' }}>
               Just Pick'em
             </Typography>
@@ -77,30 +85,5 @@ export default function AppNavbar({ isAuthenticated, username, email } : SideMen
         </Stack>
       </Toolbar>
     </AppBar>
-  );
-}
-
-export function CustomIcon() {
-  return (
-    <Box
-      sx={{
-        width: '1.5rem',
-        height: '1.5rem',
-        bgcolor: 'black',
-        borderRadius: '999px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
-        backgroundImage:
-          'linear-gradient(135deg, hsl(210, 98%, 60%) 0%, hsl(210, 100%, 35%) 100%)',
-        color: 'hsla(210, 100%, 95%, 0.9)',
-        border: '1px solid',
-        borderColor: 'hsl(210, 100%, 55%)',
-        boxShadow: 'inset 0 2px 5px rgba(255, 255, 255, 0.3)',
-      }}
-    >
-      <DashboardRoundedIcon color="inherit" sx={{ fontSize: '1rem' }} />
-    </Box>
   );
 }
